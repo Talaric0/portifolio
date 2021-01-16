8c3392fc29060fcb2c417d348d41033a03336698
bruno talarico <bruno.talarico@gmail.com>
2021-01-16 16:25:28 +0000

    typed.js done, first section finished

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Particle-js stuff
+
 particlesJS("particles-js", {
   "particles": {
     "number": {
@@ -110,22 +112,14 @@ particlesJS("particles-js", {
 });
 
 
-/* ---- stats.js config ---- */
 
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+//end of Particle-js
+
+//Typed-js stuff
+const typed = new Typed('#typed', {
+  strings: ['<h1> Bruno Talarico, ^500 <br> Software Engineer </h1>'],
+  typeSpeed: 65,
+  showCursor: false,
+  contentType: 'html'
+},
+);
